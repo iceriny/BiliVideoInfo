7cc8bf8f85d42178263b3f3f524a623bec87dbde
iceriny <suqianchengice@gmail.com>
2023-11-20 16:08:25 +0800

    修改版本号;一个简单的python 打包程序

---
+++ Packager.py
@@ -0,0 +1,49 @@
+import json
+import zipfile
+import os
+
+
+def set_manifest() -> str:
+    already_edited = False
+    with open('manifest.json', 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+        
+    package_name = data['package_name']
+    
+    input_text = input(f"当前简介:{data['description']}\n是否修改简介?\ny/n")
+    if input_text == 'y':
+        data['description'] = input("请输入简介:")
+        already_edited = True
+    
+    input_text = input(f"当前版本号为:{data['version']}\n是否修改版本号?\ny/n")
+    if input_text == 'y':
+        data['version'] = input("请输入版本号:")
+        already_edited = True
+        
+    if already_edited:
+        with open('manifest.json', 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=4, ensure_ascii=False)
+            
+    return package_name
+
+def zip_to_package(package_name : str):
+    files_list = [
+        ".\\css",
+        ".\\img",
+        ".\\scripts",
+        ".\\manifest.json"
+    ]
+    
+    with zipfile.ZipFile(f'.\\ignore\\{package_name}.zip', 'w') as zipf:
+        for path in files_list:
+            if os.path.isdir(path):
+                for root, dirs, files in os.walk(path):
+                    for file in files:
+                        file_path = os.path.join(root, file)
+                        zipf.write(file_path, os.path.relpath(file_path, '.'))
+            else:
+                zipf.write(path, os.path.basename(path))
+            
+if __name__ == '__main__':
+    package_name = set_manifest()
+    zip_to_package(package_name)
